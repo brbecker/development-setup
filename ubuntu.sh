bb87709f7437a672a3299089f143a1388680989c
Brian R. Becker <brbecker@gmail.com>
2023-11-07 00:48:38 -0800

    Fix ansible-core install

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,4 +5,4 @@ set -e
 echo "Enter your sudo password:"
 sudo apt-get update
 sudo apt-get upgrade --assume-yes
-sudo install ansible-core --assume-yes
+sudo apt-get install ansible-core --assume-yes
