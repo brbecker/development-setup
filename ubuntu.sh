81417841d146faee8cd2c57e17414f4375be5b0c
Brian R. Becker <brbecker@gmail.com>
2023-11-23 13:32:02 -0800

    Install ansible-core from ppa

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+#
+# Minimal steps to get ansible running in a fresh instance of Ubuntu.
 
-set -e
+set -ex
 
-echo "Enter your sudo password:"
+# Install ansible-core
 sudo apt-get update
-sudo apt-get upgrade --assume-yes
+sudo apt-get install software-properties-common --assume-yes
+sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt-get install ansible-core --assume-yes
+
