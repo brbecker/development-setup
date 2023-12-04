71a2a7e103d36ea9af7421fabf8796363fe1b035
Brian R. Becker <brbecker@gmail.com>
2023-12-04 14:24:54 -0800

    Install ansible-core

--- prep-docker-image.sh
+++ prep-docker-image.sh
@@ -7,6 +7,9 @@ DEFAULT_USER=brbecker
 ENC_PASSWORD='$6$fTfKjIlJdK//cPWm$dx5LWtJ.DTU5eA3ba9/iVduFrnDroow7kE9830aK40QVQfCLN5pT/JqLAlVxmmqlf24AkN3gmOQ7GmuLsF1lw.'
 USER_SHELL=/bin/bash
 
+# Timezone
+TZ=America/Los_Angeles
+
 # Create /etc/wsl.conf
 WSL_CONF=/etc/wsl.conf
 [[ -f ${WSL_CONF} ]] && mv ${WSL_CONF} ${WSL_CONF}.orig
@@ -22,3 +25,10 @@ chmod 644 ${WSL_CONF}
 # Add the user
 useradd -m -p "${ENC_PASSWORD}" -s "${USER_SHELL}" ${DEFAULT_USER}
 
+# Install ansible-core and other necessary packages
+DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND TZ
+apt-get update
+apt-get install software-properties-common --assume-yes
+add-apt-repository --yes --update ppa:ansible/ansible
+apt-get install ansible-core --assume-yes
